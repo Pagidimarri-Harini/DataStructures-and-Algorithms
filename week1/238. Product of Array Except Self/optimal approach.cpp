5f009f9d030558fe1fcc82c64780899f369a47a6
Pagidimarri-Harini <88580040+Pagidimarri-Harini@users.noreply.github.com>
2022-12-21 23:36:13 +0530

    Create optimal approach.cpp

---
+++ week1/238. Product of Array Except Self/optimal approach.cpp
@@ -0,0 +1,13 @@
+vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size(),1);
+        for(int i = 1; i < nums.size(); i++){
+            ans[i] = ans[i-1] * nums[i-1];
+        }
+        int last_prod = 1;
+        for(int i = nums.size()-1; i >= 0; i--){
+            ans[i] *= last_prod; 
+            last_prod *= nums[i];
+        }
+        return ans;
+        
+}
