a2e2a0446b6654d64bbef25595ec8840cdbfb7ca
Pagidimarri-Harini <88580040+Pagidimarri-Harini@users.noreply.github.com>
2023-04-11 14:46:32 +0530

    Create dijkstra's algo.cpp

---
+++ Graphs/shortest path in an undirected graph/dijkstra's algo.cpp
@@ -0,0 +1,24 @@
+//dijkstra's algo can't be used for negative weigted graphs, beacause it may lead to infinite loop
+// T.C: ElogV, where E is the no of edges and V is the no of vertices
+class Solution
+{
+	public:
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        priority_queue<pair<int,int> , vector<pair<int,int>>, greater<pair<int,int>> > pq;
+        vector<int> distance(V, INT_MAX);
+        distance[S] = 0;
+        pq.push({0,S});
+        while(!pq.empty()){
+            pair<int, int> x = pq.top();
+            pq.pop();
+            for(auto ad: adj[x.second]){
+                if(distance[ad[0]] > ad[1] + x.first){
+                    distance[ad[0]] = ad[1] + x.first;
+                    pq.push({distance[ad[0]], ad[0]});
+                }
+            }
+        }
+        return distance;
+    }
+};
