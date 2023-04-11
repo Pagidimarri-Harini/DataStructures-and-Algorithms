1ccaf24e997269cba7d3e6c1ef90f027092e6807
Pagidimarri-Harini <88580040+Pagidimarri-Harini@users.noreply.github.com>
2023-04-11 15:19:11 +0530

    Update setdijkstra's.cpp

--- Graphs/shortest path in an undirected graph/setdijkstra's.cpp
+++ Graphs/shortest path in an undirected graph/setdijkstra's.cpp
@@ -3,17 +3,20 @@ class Solution
 	public:
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-        priority_queue<pair<int,int> , vector<pair<int,int>>, greater<pair<int,int>> > pq;
+        set<pair<int, int>>s;
         vector<int> distance(V, INT_MAX);
         distance[S] = 0;
-        pq.push({0,S});
-        while(!pq.empty()){
-            pair<int, int> x = pq.top();
-            pq.pop();
+        s.insert({0,S});
+        while(!s.empty()){
+            pair<int, int> x = *(s.begin());
+            s.erase(x);
             for(auto ad: adj[x.second]){
                 if(distance[ad[0]] > ad[1] + x.first){
+                    if(distance[ad[0]] != INT_MAX){
+                        s.erase({distance[ad[0]], ad[0]});
+                    }
                     distance[ad[0]] = ad[1] + x.first;
-                    pq.push({distance[ad[0]], ad[0]});
+                    s.insert({distance[ad[0]], ad[0]});
                 }
             }
         }
