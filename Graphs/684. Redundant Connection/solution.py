296d2d5a67c8e4a9ec84ac041a4bcc9ef906e454
Pagidimarri-Harini <88580040+Pagidimarri-Harini@users.noreply.github.com>
2023-04-16 21:39:37 +0530

    Create solution.py

---
+++ Graphs/684. Redundant Connection/solution.py
@@ -0,0 +1,30 @@
+class DisjointSet:
+    def __init__(self,n):
+        self.parent = [i for i in range(n)]
+        self.size = [1]*n
+        self.count = 0
+    def findParent(self, node):
+        if self.parent[node] == node:
+            return node
+        self.parent[node] = self.findParent(self.parent[node])
+        return self.parent[node]
+    def union(self, n1, n2):
+        p1 = self.findParent(n1)
+        p2 = self.findParent(n2)
+        if p1 == p2:
+            self.count += 1
+        elif self.size[p1] < self.size[p2]:
+            self.parent[p1] = self.parent[p2]
+            self.size[p2] += self.size[p1]
+        else:
+            self.parent[p2] = self.parent[p1]
+            self.size[p2] += self.size[p1]
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        ds = DisjointSet(len(edges))
+        for i in edges:
+            if ds.findParent(i[0]-1) == ds.findParent(i[1]-1):
+                return [i[0], i[1]]
+            else:
+                ds.union(i[0]-1, i[1]-1)
+
