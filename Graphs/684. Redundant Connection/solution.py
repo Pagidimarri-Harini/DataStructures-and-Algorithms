be73a79412c4461350db6f54df1bd900db8a0d0c
Pagidimarri-Harini <88580040+Pagidimarri-Harini@users.noreply.github.com>
2023-04-17 19:34:30 +0530

    Update solution.py

--- Graphs/684. Redundant Connection/solution.py
+++ Graphs/684. Redundant Connection/solution.py
@@ -17,7 +17,7 @@ class DisjointSet:
             self.size[p2] += self.size[p1]
         else:
             self.parent[p2] = self.parent[p1]
-            self.size[p2] += self.size[p1]
+            self.size[p1] += self.size[p2]
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         ds = DisjointSet(len(edges))
